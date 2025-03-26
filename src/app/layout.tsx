3cc2a83dc4516eab0c642a8ee4ee209afb7ddd00
ZITZ-bit <zitzcode@gmail.com>
2025-03-26 09:23:48 -0400

    Inicio | Proyecto

---
+++ src/app/layout.tsx
@@ -0,0 +1,16 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Babel | Inicio",
+  description: "Informacion Principal",
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,21 @@
+import "../styles/style.css"
+
+export default function Inicio() {
+  return (
+    <section>
+      <header>
+
+      </header>
+
+      <main>
+        <section>
+
+        </section>
+      </main>
+
+      <footer>
+
+      </footer>
+    </section>
+  );
+}
